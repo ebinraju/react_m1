529b9616db50079328a5397f918107e8279c630d
Ebin.r <ebin.r@thinkpalm.com>
2024-05-21 10:41:09 +0530

    initial commit

---
+++ src/components/navigation/childrens/navbarConst.js
@@ -0,0 +1,27 @@
+export const modules = [
+    {
+        name : "m1 module 1",
+        URL : "/home",
+        accessValue : true,
+    },
+    {
+        name : "m1 module 2",
+        URL : "/login",
+        accessValue : true,
+    },
+    {
+        name : "m1 module 3",
+        URL : "/dashboard",
+        accessValue : false,
+    },
+    {
+        name : "m1 module 4",
+        URL : "/home",
+        accessValue : true,
+    },
+    {
+        name : "m1 module 5",
+        URL : "/dashboard",
+        accessValue : false,
+    },
+];
